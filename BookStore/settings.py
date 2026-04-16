697aecee0167a29ae33466c4adafcf1374fadb9f
Satheesh Kumar <gskrocktamilann@gmail.com>
2026-04-16 21:18:52 +0530

    fix templates path

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR=os.path.join(BASE_DIR,'Templates')
+TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
